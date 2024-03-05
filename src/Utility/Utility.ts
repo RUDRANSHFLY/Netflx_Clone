94ebdf627f5e49cff54863c4004851012247cacb
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-05 15:15:59 +0530

    refactor: remove unused utility variables

--- src/Utility/Utility.ts
+++
@@ -1,3 +0,0 @@
-export const Netflix_Logo : string = 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png';
-
-export const Netflix_Avatar : string = 'https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg';
