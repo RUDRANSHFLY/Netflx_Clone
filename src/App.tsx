e3b517e9ea6d7d631cda61ddc245556efa31aa37
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-09 17:12:10 +0530

    feat: Add login functionality and routing to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
+import LogIn from "./components/auth/logIn/LogIn";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = null;
+
   return (
     <>
       <div className="app">
-        <Home />
+        {!user ? (
+          <LogIn />
+        ) : (
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Router>
+        )}
       </div>
     </>
   );
