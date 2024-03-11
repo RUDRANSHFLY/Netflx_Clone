78a3070ece5d872314fe58d553496dcad3390415
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-11 16:57:58 +0530

    feat: Add user authentication functionality
    This commit adds user authentication functionality using Firebase. It includes importing necessary dependencies, setting up the authentication listener, and dispatching actions to log in or log out the user based on their authentication status. It also adds a new route for the user page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import LogIn from "./components/auth/logIn/LogIn";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { auth } from "./fireBase/FireBase";
+import { logIn, logOut, selectUser } from "./stateManagement/userReducer";
+import Profile from "./components/profile/Profile";
 
 function App() {
-  const user = false;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          logIn({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
+        dispatch(logOut());
+      }
+    });
+
+    return () => {
+      unSubscribe;
+    };
+  }, [dispatch]);
 
   return (
     <>
@@ -15,6 +40,7 @@ function App() {
           <Router>
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
             </Routes>
           </Router>
         )}
