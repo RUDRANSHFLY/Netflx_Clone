a5ece06581c1069a01b0071a7e9fadb57fdd69bb
RUDRANSHFLY <rokudokubaradiyabrothers@gmail.com>
2024-03-11 16:59:45 +0530

    feat: Add navigation functionality to navbar
    - Added onClick event to Netflix logo and Netflix avatar images in the Navbar component to navigate to the home page and profile page respectively.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { Netflix_Avatar, Netflix_Logo } from "../Utility/Utility";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const [show, setShow] = useState(false);
+  const navigate = useNavigate();
 
   const transitionNavBar = () => {
     if (window.scrollY > 100) {
@@ -26,12 +28,19 @@ const Navbar = () => {
         show ? "bg-black" : "bg-transparent"
       } z-50 w-screen fixed pt-4 pb-4 px-12 flex flex-row justify-between items-center`}
     >
-      <img className="h-10" src={Netflix_Logo} alt="" srcSet="" />
+      <img
+        onClick={() => navigate("/")}
+        className="h-10 hover:cursor-pointer"
+        src={Netflix_Logo}
+        alt=""
+        srcSet=""
+      />
       <img
         className="h-10 cursor-pointer"
         src={Netflix_Avatar}
         alt=""
         srcSet=""
+        onClick={() => navigate("/profile")}
       />
     </div>
   );
